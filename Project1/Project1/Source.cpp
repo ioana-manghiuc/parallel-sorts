491d604f43a00f67a3816d4c80e6c83e856e0919
ioana manghiuc <147768804+ioana-manghiuc@users.noreply.github.com>
2025-04-23 19:38:36 +0300

    Recorded --> Direct (2-4-8/1mil)

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -18,8 +18,8 @@ int main(int argc, char** argv)
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	int n = 10'000'000;
-	std::ifstream data_file("data_10mil.txt", std::ifstream::in);
+	int n = 1'000'000;
+	std::ifstream data_file("data_1mil.txt", std::ifstream::in);
 
 	std::vector<int>data(n);
 	if (rank == 0)
@@ -36,7 +36,7 @@ int main(int argc, char** argv)
 	double computation_time = 0.0, communication_time = 0.0;
 	double execution_time = MPI_Wtime();
 
-	MPI_BucketSort(data, rank, size, computation_time, communication_time);
+	MPI_DirectSort(data, rank, size, computation_time, communication_time);
 
 	execution_time = MPI_Wtime() - execution_time;
 	if (rank == 0)
